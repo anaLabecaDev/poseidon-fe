ca4432241a57929fa3125cd0ae5e21ebe323249d
Ana Labeca <anafrost01@gmail.com>
2024-07-24 16:09:19 +0100

    chore: update env files and other configs

---
+++ src/config/env-schema.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod'
+
+export const EnvSchema = z.object({
+  API_URL: z.string(),
+  ENABLE_API_MOCKING: z
+    .string()
+    .refine((s) => s === 'true' || s === 'false')
+    .transform((s) => s === 'true')
+    .optional(),
+  APP_URL: z.string().optional().default('http://localhost:3000'),
+  APP_MOCK_API_PORT: z.string().optional().default('8080'),
+  CLIENT_ID: z.string(),
+  TENANT_ID: z.string(),
+})
+
+export type EnvSchemaType = z.infer<typeof EnvSchema>
